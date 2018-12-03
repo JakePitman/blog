9d0304922342fd7946ea237843f15bab92d47707
Jay Pitman <jakeseanp@gmail.com>
2018-12-04 10:33:39 +1100

    add subtitle to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -46,6 +46,6 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:title, :content)
+    params.require(:post).permit(:title, :subtitle, :content)
   end
 end
