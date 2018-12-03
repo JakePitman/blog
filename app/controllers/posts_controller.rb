fc5b7638c45da6de5f23daab3384cd130d318979
Jay Pitman <jakeseanp@gmail.com>
2018-12-03 19:23:11 +1100

    add posts#new and _form.html.erb partial

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,30 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: 'Post saved successfully' }
+      else
+        format.html { render new }
+      end
+    end
+  end
+
   private
 
   def set_post
     @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'post/:id', to: 'posts#show', as: 'post_show'
+  get 'post/:id', to: 'posts#show', as: 'post'
   get 'posts', to: 'posts#index', as: 'posts'
+  get 'posts/new', to: 'posts#new'
+  post 'posts', to: 'posts#create'
   root to: 'pages#home'
 end
