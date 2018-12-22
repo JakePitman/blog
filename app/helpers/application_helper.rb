7fbe85da80de59cba20cde4a1c97ee8e9017615c
Jay Pitman <jakeseanp@gmail.com>
2018-12-22 16:44:45 +1100

    add additional logic to character_limit helper, for cases with long single words

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,8 @@
 module ApplicationHelper
   def character_limit(string, maximum_characters)
+    string.split(' ').each do |word|
+      return string[0..maximum_characters / 2.5] + '...' if word.length > (maximum_characters / 2)
+    end
     string.length > maximum_characters ? string[0..maximum_characters] + '...' : string
   end
 end
