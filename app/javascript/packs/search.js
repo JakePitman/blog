0f75f9f4898ae19cee5b880cb6abb45f3b5f1822
Jay Pitman <jakeseanp@gmail.com>
2018-12-28 19:43:35 +1100

    refactor topics list in profile_show to React search component

--- app/javascript/packs/search.js
+++ app/javascript/packs/search.js
@@ -3,11 +3,31 @@ import ReactDOM from 'react-dom';
 
 class Search extends React.Component {
 
+  characterLimit = (string, maximumCharacters) => {
+    let result = null
+    if (string.length > maximumCharacters) {
+      result = string.substr(0, maximumCharacters) + '...'
+    } else {
+      result =  string
+    }
+    string.split(' ').forEach(word => {
+      if ( word.length > (maximumCharacters / 2) ) {
+        result = string.substr(0, (maximumCharacters / 2.5)) + '...'
+      }
+    })
+    return result
+  }
+
   topicsData = () => {
 
     let topics = []
     this.props.topics.forEach(topic => {
-      topics.push(<h1>{topic.title}</h1>)
+      topics.push(
+        <div className='profile-page-link-container'>
+          <div className='color-fill'></div>
+          <a className='profile-page-link-inner-content' href={`/topic/${topic.id}`}>{this.characterLimit(topic.title, 20)}</a>
+        </div>
+      )
     })
 
     return topics
@@ -15,8 +35,7 @@ class Search extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>TITLE:</h1>
+      <div className='profile-page-links-container'>
         {this.topicsData()}
       </div>
     )
